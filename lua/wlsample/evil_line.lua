16e63a561abf7a1e4d5ce5e54ccc45e4e700f5b8
oeyoews <jyao4783@gmail.com>
2022-06-18 23:48:19 +0800

    🎸 This is an automatic push

--- lua/wlsample/evil_line.lua
+++ lua/wlsample/evil_line.lua
@@ -115,10 +115,10 @@ basic.git = {
   text = function(bufnr)
     if git_comps.is_git(bufnr) then
       return {
-        --        
+        --        
         { git_comps.diff_added({ format = '  %s', show_zero = true }), 'green' },
         { git_comps.diff_removed({ format = '  %s', show_zero = true }), 'red' },
-        { git_comps.diff_changed({ format = "  %s", show_zero = true }), "blue" },
+        { git_comps.diff_changed({ format = "  %s", show_zero = true }), "blue" },
       }
     end
     return ''

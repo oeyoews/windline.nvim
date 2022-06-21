f114f51ae7e23f0311313b537a90711749a4a490
oeyoews <jyao4783@gmail.com>
2022-06-21 11:54:10 +0800

    🎸 This is an automatic push

--- lua/windline/components/lsp.lua
+++ lua/windline/components/lsp.lua
@@ -25,7 +25,7 @@ local lsp_client_names = function(bufnr, opt)
   opt = opt or {}
   local clients = {}
   -- local icon = opt.icon or '  '         
-  local icon = opt.icon or ' 👀' -- 👀
+  local icon = opt.icon or '  ' -- 👀 
   local sep = opt.separator or ' & '
 
   for _, client in pairs(vim.lsp.buf_get_clients(bufnr or 0)) do

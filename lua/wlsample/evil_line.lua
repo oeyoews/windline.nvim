97db351f0c7ee24b5b40da78126970c1e467c051
oeyoews <jyao4783@gmail.com>
2022-07-27 11:13:08 +0800

    fix(navic): remove navic

--- lua/wlsample/evil_line.lua
+++ lua/wlsample/evil_line.lua
@@ -28,24 +28,24 @@ local colors_mode = {
 
 -- TODO add adjust this plugin
 -- TODO use api
-local navic_comp = "%{%v:lua.require'nvim-navic'.get_location()%}"
+-- local navic_comp = "%{%v:lua.require'nvim-navic'.get_location()%}"
 
-basic.navic_comp = {
-  hl_colors = {
-    red = { "red", "black" },
-    yellow = { "yellow", "black" },
-    blue = { "blue", "black" },
-    green = { "green", "black" },
-  },
-  text = function(_, _, width)
-    if width > 90 then
-      return {
-        { " ", "red" },
-        { navic_comp },
-      }
-    end
-  end,
-}
+-- basic.navic_comp = {
+--   hl_colors = {
+--     red = { "red", "black" },
+--     yellow = { "yellow", "black" },
+--     blue = { "blue", "black" },
+--     green = { "green", "black" },
+--   },
+--   text = function(_, _, width)
+--     if width > 90 then
+--       return {
+--         { " ", "red" },
+--         { navic_comp },
+--       }
+--     end
+--   end,
+-- }
 
 -- local time = os.date("%x %X")
 -- local time = os.date("%y/%m/%d %H:%M ")
@@ -231,7 +231,7 @@ local default = {
     basic.vi_mode,
     -- basic.file,
     basic.lsp_diagnos, -- if put it after lsp_name, maybe have bug, fix in #xxx TODO
-    basic.navic_comp,
+    -- basic.navic_comp,
     basic.divider,
     basic.file_right,
     basic.lsp_name,

60f87848ed7fc8118c3d112bae85f3b36caf2fd5
oeyoews <jyao4783@gmail.com>
2022-07-30 12:46:21 +0800

    robot: This is an automatic push

--- lua/windline/components/lsp.lua
+++ lua/windline/components/lsp.lua
@@ -24,8 +24,7 @@ end
 local lsp_client_names = function(bufnr, opt)
 	opt = opt or {}
 	local clients = {}
-	-- local icon = opt.icon or '  '            🟨   
-	local icon = opt.icon or " 𝑆 " -- 👀     🔥  漣😀 煉     ie漣    
+	local icon = opt.icon or "  "
 	-- local sep = opt.separator or "   "
 	local sep = opt.separator or " "
 
